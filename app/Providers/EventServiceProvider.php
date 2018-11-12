1c79b0d61c8f51cd0632523dcdd0f6e0cbaceff0
wau <dima.yerashkin@gmail.com>
2018-11-12 11:53:52 +0600

    Removes implementation via listener+event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,9 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        'App\Events\TaskCreated' => [
-            'App\Listeners\ProcessTask',
-        ],
     ];
 
     /**
